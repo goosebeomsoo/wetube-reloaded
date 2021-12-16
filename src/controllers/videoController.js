ecdb8222cd53f103c1183d9d8a36ecbeece15196
goosebeomsoo <rlaqjatn975@gmail.com>
2021-12-16 22:01:01 +0900

    All-That-Pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,20 +1,10 @@
-export const trending = (req, res) => res.render("home");
-
-export const see = (req, res) => {
-    return res.render("watch");
-};
-
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("edit");
-};
-
+export const trending = (req, res) => res.render("home", {pageTitle: "HOME"});
+// HTML에 변수 전달
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
-
 export const upload = (req, res) => res.send("upload");
-
-export const deleteVideo = (req, res) => {
-    console.log(req.params);
+export const deleteVideo = (req, res) =>{
     return res.send("deleteVideo");
 }
 

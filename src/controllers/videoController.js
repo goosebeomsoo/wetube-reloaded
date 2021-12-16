0ee3ae2fa5307b4094d1e1273db3cf92dcc04a94
goosebeomsoo <rlaqjatn975@gmail.com>
2021-12-16 14:27:24 +0900

    URL-Routers

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,20 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+
+export const see = (req, res) => {
+    console.log(req.params);
+    return res.send(`watch video #${req.params.id}`);
+};
+
+export const edit = (req, res) => {
+    console.log(req.params);
+    return res.send("edit");
+};
+
 export const search = (req, res) => res.send("search");
+
 export const upload = (req, res) => res.send("upload");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+
+export const deleteVideo = (req, res) => {
+    console.log(req.params);
+    return res.send("deleteVideo");
+}
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,21 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController"
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
+videoRouter.get("/:id(\\d+)", see);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
+
+/*
+parameter (/:[url])
+- url안에 변수를 포함시킬 수 있게 해줌
+- parameter를 포함한 라우터는 아래로 위치하게
+
+Express Router
+
+Regular Expression(정규식)
+- 문자열로부터 특정 정보를 추출해내는 방식
+- 모든 프로그래밍 언어에 존재
+- (\\d+) : 숫자만 선택하는 정규식
+*/

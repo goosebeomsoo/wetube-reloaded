b0e2e5fc8ff0ad264e0174d8d0717ed900ead6b6
goosebeomsoo <rlaqjatn975@gmail.com>
2022-01-11 14:25:22 +0900

    mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,20 @@
 // entry -> 우리가 처리하고자하는 파일
 console.log(typeof __dirname);
 // __dirname : 파일까지의 전체경로
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// css를 추출해서 별도의 파일로 만들어줌
 const path = require("path");
 // path.resolve는 입력하는 파트들을 모아서 경로로 만들어줌.
 console.log(path.resolve(__dirname, "assets","js"));
 module.exports = {
     entry : "./src/client/js/main.js",
+    plugins : [new MiniCssExtractPlugin({ 
+        filename : "css/styles.css" 
+    })],
     mode : "development",
     output : {
-        filename : "main.js",
-        path : path.resolve(__dirname, "assets","js"),
+        filename : "js/main.js",
+        path : path.resolve(__dirname, "assets"),
         // absolute path로 적어야함 : 처음부터 끝까지의 경로로
     },
     module : {
@@ -26,7 +31,9 @@ module.exports = {
             },
             {
                 test : /\.scss$/,
-                use : ["style-loader", "css-loader", "sass-loader"],
+                use : [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                // sass-loader는 scss를 css로 변환시키는 역할
+                // style-loader는 css를 브라우저에 적용시키는 역할
             }
         ],
     },

cae0aa6d76a74face2f55fd6d6104f2022002f9f
goosebeomsoo <rlaqjatn975@gmail.com>
2022-01-11 12:36:25 +0900

    Webpack-Setting

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+// 오래된 JavaScript Version만 인식을함
+// entry -> 우리가 처리하고자하는 파일
+module.exports = {
+    entry : "./src/client/js/main.js",
+    output : {
+        filename : "main.js",
+        path : "./assets/js"
+    }
+     // 우리가 처리하고자하는 파일
+}

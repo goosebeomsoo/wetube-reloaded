9e895189763a69417f8186acac67511dd50d9460
goosebeomsoo <rlaqjatn975@gmail.com>
2022-01-21 11:20:39 +0900

    Cleaning Code

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,13 +2,33 @@
 import {createFFmpeg, fetchFile} from "@ffmpeg/ffmpeg";
 
 const video = document.querySelector("#preview");
-const startBtn = document.querySelector("#startBtn");
+const actionBtn = document.querySelector("#actionBtn");
 
 let stream;
 let recorder;
 let videoFile;
 
+const files = {
+    input : "recording.webm",
+    output : "output.mp4",
+    thumb : "thumbnail.jpg",
+}
+
+const downloadFile = (fileUrl, fileName) => {
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    // The HTMLAnchorElement.download property is a DOMString indicating that the linked resource is intended to be downloaded rather than displayed in the browser.
+}
+
 const handleDownload = async () => {
+
+    actionBtn.removeEventListener("click", handleDownload);
+    actionBtn.innerText = "Transcoding";
+    actionBtn.disabled = true;
+
     const ffmpeg = createFFmpeg({ 
         corePath: "https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js",
         log:true,
@@ -16,15 +36,15 @@ const handleDownload = async () => {
     await ffmpeg.load();
     // User가 JavaScript가 아닌 설치된 프로그램을 이용하기때문에 await 사용
 
-    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+    ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
     // writeFile은 ffmpeg에 가상의 파일을 생성해줌
-    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+    await ffmpeg.run("-i", files.input, "-r", "60", files.output);
     // unsigned integer은 양의 정수를 의미
     // signed 음의 정수
-    await ffmpeg.run("-i", "recording.webm", "-ss", "00:00:01", "-frames:v", "1", "thumbnail.jpg");
+    await ffmpeg.run("-i", files.input, "-ss", "00:00:01", "-frames:v", "1", files.thumb);
 
-    const mp4File = ffmpeg.FS("readFile", "output.mp4");
-    const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+    const mp4File = ffmpeg.FS("readFile", files.output);
+    const thumbFile = ffmpeg.FS("readFile", files.thumb);
 
     const mp4Blob = new Blob([mp4File.buffer], {type : "video/mp4"});
     const thumbBlob = new Blob([thumbFile.buffer], {type : "image/jpg"});
@@ -32,43 +52,40 @@ const handleDownload = async () => {
     const mp4Url = URL.createObjectURL(mp4Blob);
     const thumbUrl = URL.createObjectURL(thumbBlob);
     
-    const mp4A = document.createElement("a");
-    mp4A.href = mp4Url;
-    mp4A.download = "MyRecording.mp4";
-    document.body.appendChild(mp4A);
-    mp4A.click();
 
-    const thumbA = document.createElement("a");
-    thumbA.href = thumbUrl;
-    thumbA.download = "MyRecordingThumb.jpg";
-    document.body.appendChild(thumbA);
-    thumbA.click();
-    // The HTMLAnchorElement.download property is a DOMString indicating that the linked resource is intended to be downloaded rather than displayed in the browser.
+    downloadFile(mp4Url ,"MyRecording.mp4");
+    // Download video
+    downloadFile(thumbUrl ,"MyRecordingThumb.jpg");
+    // Download thumbnail
 
-    ffmpeg.FS("unlink", "recording.webm");
-    ffmpeg.FS("unlink", "output.mp4");
-    ffmpeg.FS("unlink", "thumbnail.jpg");
+    ffmpeg.FS("unlink", files.input);
+    ffmpeg.FS("unlink", files.output);
+    ffmpeg.FS("unlink", files.thumb);
     // Remove transcoded file
 
     URL.revokeObjectURL(mp4Url);
     URL.revokeObjectURL(thumbUrl);
     URL.revokeObjectURL(videoFile);
     // Remove file link
+
+    actionBtn.disabled = false;
+    actionBtn.innerText = "Recording Again";
+    actionBtn.addEventListener("click", handleStart);
 }
 
 const handleStop = () => {
-    startBtn.innerText = "Download Video";
-    startBtn.removeEventListener("click", handleStop);
-    startBtn.addEventListener("click", handleDownload);
+    actionBtn.innerText = "Download Video";
+    actionBtn.removeEventListener("click", handleStop);
+    actionBtn.addEventListener("click", handleDownload);
     recorder.stop();
     // Stops recording, at which point a dataavailable event containing the final Blob of saved data is fired. No more recording occurs.
     // BLOB (Binary Large Object) : 이진 데이터 모임.
 }
 
 const handleStart = () => {
-    startBtn.innerText = "Stop Recording";
-    startBtn.removeEventListener("click", handleStart);
-    startBtn.addEventListener("click", handleStop);
+    actionBtn.innerText = "Stop Recording";
+    actionBtn.removeEventListener("click", handleStart);
+    actionBtn.addEventListener("click", handleStop);
 
     recorder = new MediaRecorder(stream, {mimeType : "video/webm"});
     // creates a new MediaRecorder object that will record a specified MediaStream
@@ -102,4 +119,4 @@ const init = async () => {
 
 init();
 
-startBtn.addEventListener("click", handleStart);
+actionBtn.addEventListener("click", handleStart);

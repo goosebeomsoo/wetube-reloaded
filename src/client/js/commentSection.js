8926e8d0eef8e2120f6845b2e5279d49a6614b31
goosebeomsoo <rlaqjatn975@gmail.com>
2022-01-25 12:07:04 +0900

    Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,19 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+    const span = document.createElement("span")
+    span.innerText = `${text}`;
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.prepend(newComment);
+}
+
 handleSubmit = async (e) => {
     e.preventDefault();
     const textarea = form.querySelector("textarea");
@@ -9,7 +22,7 @@ handleSubmit = async (e) => {
     if (text === "") {
         return;
     }
-    await fetch(`/api/videos/${videoId}/comment`, {
+    const {status} = await fetch(`/api/videos/${videoId}/comment`, {
         // fetch가 Backend에 갔다가 다시 돌아와서 작업하기 때문에 await를 해줘야함
         method : "POST",
         headers : {
@@ -18,6 +31,9 @@ handleSubmit = async (e) => {
         body : JSON.stringify({text}),
     });
     textarea.value = "";
+    if (status===201) {
+        addComment(text);
+    }
     // window.location.reload();
     // 새로고침
 };
